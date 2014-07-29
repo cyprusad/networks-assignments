2e64bba9296826e7b75f71d044bcc38a4d5234cb
Sai Warang <cyprusad@gmail.com>
2014-07-29 01:41:17 -0400

    basic logging

--- a3/router.c
+++ a3/router.c
@@ -1,11 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
 
 #include "router.h"
 
-int send_init() {
+char filename[64]; // global filename string
+
+
+int router_log(char* data) {
+  int res = 0;
+  if (filename != NULL) {
+    printf("log - Filename is defined %s\n", filename);
+    FILE* fp = fopen(filename, "ab");
+    printf("log - %s\n", data);
+    if (fp != NULL) {
+      if (fputs(data, fp) != EOF) {
+        res = 1;
+      }
+      fclose(fp);
+    }
+  }
+  return res; //return 1 if things went well
+}
+
+
+int send_init(router_id) {
   // send init package to the network emulator
+  router_log("INIT");
   return 0;
 }
 
@@ -29,10 +57,14 @@ int main (int argc, char** argv) {
     strcpy(nse_host, argv[2]);
     nse_port = atoi(argv[3]);
     router_port = atoi(argv[4]);
-    printf("router::main - Starting the router\n");
-  
-  }
+    printf("main - Starting the router\n");
 
- 
+    // set filename
+    sprintf(filename, "router%d.log", router_id);
+
+    send_init(router_id);
+
+
+  } 
   return 0;
 }
